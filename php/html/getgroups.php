2baf011fceee0befe39a838cc2deadd8ba0228dc
ivandeex <ivandeex@gmail.com>
2010-12-13 12:46:46 +0000

    return list of groups

---
+++ php/html/getgroups.php
@@ -0,0 +1,17 @@
+<?php
+// $Id$
+
+// Return list of users from LDAP
+
+require '../lib/common.php';
+
+configure();
+ldap_connect_to('uni');
+$res = ldap_search_for('uni', "(objectClass=posixGroup)", array('cn'));
+$msg = get_error();
+if (empty($res) && !empty($msg)) {
+    echo "{success:false,message:".json_encode($msg)."}";
+} else {
+    echo "{success:true,rows:".json_encode($res)."}";
+}
+?>
